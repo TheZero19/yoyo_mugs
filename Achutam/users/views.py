b302a8f5877930ebab8d5aaa4464aa20c3c14d29
SampurnaKC <sampurnakc19@gmail.com>
2024-02-23 20:26:44 +0545

    orders.html loaded with respect to the user status + orders dynamically loaded from database with respect to the form filled by the normal users for the orders

--- Achutam/users/views.py
+++ Achutam/users/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
-from restaurant.models import Order
+from restaurant.models import Order, MenuItem, MenuItemType
 
 # Create your views here.
 def register(request):
@@ -38,8 +38,34 @@ def users_logout(request):
     return render(request, "users/logout.html")
 
 def users_orders(request):
-    orders = Order.objects.all()
-    for order in orders:
-        total_price = sum(item.itemPPU for item in order.items.all())
-        setattr(order, 'total_price', total_price)
-    return render(request, "users/orders.html", context={'orders': orders})
+    print("View function called")
+    if request.method == "POST":
+        selected_items = request.POST.getlist('items')
+        print(selected_items)
+        total_price = 0  # Initialize total price
+
+        items = []
+        for selected_item in selected_items:
+            item = MenuItem.objects.filter(itemName__iexact = selected_item).first()
+            total_price+=item.itemPPU
+            items.append(item)
+
+        print(items)
+        print(total_price)
+        # Create a new order
+        order = Order.objects.create(
+            customer=request.user,
+            total_price=total_price,
+            isPending=True  # Assuming the order is pending by default
+        )
+        order.items.set(items)
+        messages.success(request, "Your order has been requested!")
+        return HttpResponse(total_price)
+    else:
+        orders = Order.objects.all()
+        menu_item_types = MenuItemType.objects.all()
+        menu_items = MenuItem.objects.all()
+        for order in orders:
+            total_price = sum(item.itemPPU for item in order.items.all())
+            setattr(order, 'total_price', total_price)
+        return render(request, "users/orders.html", context={'orders': orders, 'menu_items': menu_items, 'menu_item_types': menu_item_types})
